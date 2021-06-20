89ea99d7394ea2a471d39b8973430af7cf8710e3
Nnamdi <nnwabuokei@paga.com>
2021-06-20 07:23:08 +0100

    feat(distance-locator):set up LocationEntity as a feature

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { LocationController } from './location.controller';
+import LocationEntity from './entities/location.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LocationEntity])],
   controllers: [LocationController],
   providers: [LocationService]
 })
