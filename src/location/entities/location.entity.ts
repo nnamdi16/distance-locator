37684e163d2afd6d8ef6c095a62bbfd0191937dc
Nnamdi <nnwabuokei@paga.com>
2021-06-20 07:25:56 +0100

    feat(distance-locator):Implemented the location entity(schema)

--- src/location/entities/location.entity.ts
+++ src/location/entities/location.entity.ts
@@ -1,1 +1,47 @@
-export class Location {}
+import { BaseEntity } from "src/shared/base.entity";
+import { Column, Index } from "typeorm";
+import {Point } from 'geojson'
+
+class LocationEntity extends BaseEntity{
+
+  @Column()
+  public description: string;
+
+  @Column({ nullable: true })
+  public website?: string;
+
+  @Column()
+  public phoneNumber: string;
+
+  @Column()
+  public contactPerson: string;
+
+  @Column()
+  public sourceName: string;
+
+  @Column()
+  public destinationName: string;
+
+  @Column()
+  public distance: number;
+
+  @Index({ spatial: true })
+  @Column({
+    type: 'geography',
+    spatialFeatureType: 'Point', 
+    srid: 4326,
+    nullable: true,
+  })
+  public sourceCoordinates: Point;
+
+  @Index({ spatial: true })
+  @Column({
+    type: 'geography',
+    spatialFeatureType: 'Point', 
+    srid: 4326,
+    nullable: true,
+  })
+  public destinationCoordinates: Point;
+ }
+ 
+export default LocationEntity
