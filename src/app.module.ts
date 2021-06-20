8df27f4709973a64bdb20da1a5cb572070772ad0
Nnamdi <nnwabuokei@paga.com>
2021-06-20 07:18:24 +0100

    feat(distance-locator):Added validations for database environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationModule } from './location/location.module';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [LocationModule],
+  imports: [    ConfigModule.forRoot({
+    validationSchema: Joi.object({
+      POSTGRES_HEROKU_HOST: Joi.string().required(),
+      POSTGRES_PORT: Joi.number().required(),
+      POSTGRES_HEROKU_USER: Joi.string().required(),
+      POSTGRES_HEROKU_PASSWORD: Joi.string().required(),
+      POSTGRES_HEROKU_DB: Joi.string().required(),
+      PORT: Joi.number()
+    }),
+  }),
+  DatabaseModule,
+  ,LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
