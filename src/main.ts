6c654bf97438c13f593ad3bf598c9f88339131bb
Nnamdi <nnwabuokei@paga.com>
2021-06-20 07:21:52 +0100

    feat(distance-locator): enabled cors and set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['debug','error', 'log', 'verbose', 'warn']
+  });
   await app.listen(3000);
+  app.enableCors();
+  app.use(helmet());
+  const config = new DocumentBuilder()
+    .setTitle('Distance Locator')
+    .setDescription('REST API sercices calculating distance between two locations and storing location details')
+    .setVersion('1.0')
+    .addTag("distance-locator")
+    .addBearerAuth()
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
+  Logger.log(`Server started on http://localhost:${port}, Boostrap`);
+
 }
 bootstrap();
