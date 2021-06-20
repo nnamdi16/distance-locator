c9b8b2e7089479ed969cf3bcbe034d320d79b420
Nnamdi <nnwabuokei@paga.com>
2021-06-20 13:23:18 +0100

    feat(distance-locator): refactored app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,26 @@
+import { LocationModule } from './location/location.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LocationModule } from './location/location.module';
 import * as Joi from '@hapi/joi';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [    ConfigModule.forRoot({
-    validationSchema: Joi.object({
-      POSTGRES_HEROKU_HOST: Joi.string().required(),
-      POSTGRES_PORT: Joi.number().required(),
-      POSTGRES_HEROKU_USER: Joi.string().required(),
-      POSTGRES_HEROKU_PASSWORD: Joi.string().required(),
-      POSTGRES_HEROKU_DB: Joi.string().required(),
-      PORT: Joi.number()
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HEROKU_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_HEROKU_USER: Joi.string().required(),
+        POSTGRES_HEROKU_PASSWORD: Joi.string().required(),
+        POSTGRES_HEROKU_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
     }),
-  }),
-  DatabaseModule,
-  ,LocationModule],
+    DatabaseModule,
+    LocationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
